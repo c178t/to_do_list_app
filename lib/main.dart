1eaefc2ad5597433c0e246ca84bde86f7a76d680
Cian Thomas <cianticloud@gmail.com>
2023-07-11 11:23:52 -0400

    Moved provider

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,14 @@ import 'package:to_do_list_app/provider.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyApp(),
-    )
+    ChangeNotifierProvider(
+      create: (context) => ToDoListProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const MyApp(),
+      )
+    ),
+    
   );
 }
 
@@ -30,78 +34,71 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => ToDoListProvider(),
-        )
-      ],
-      child: ScreenUtilInit(
-        builder: (context, child) => MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: Scaffold(
-            body: Center(child: pages[pageIndex]),
-            backgroundColor: Color.fromARGB(255, 255, 255, 255),
-            bottomNavigationBar: 
-              BottomNavigationBar(
-                onTap: (index) => setState(() {
-                  pageIndex = index;
-                }),
-                currentIndex: pageIndex,
-                backgroundColor: Color.fromARGB(255, 237, 234, 234),
-                items: [
-                  BottomNavigationBarItem(
-                    icon: Padding(padding: EdgeInsets.fromLTRB(0, 10.h, 0, 0),
-                    child: Icon(Icons.home, 
-                    color: Color.fromARGB(255, 68, 68, 68),
-                    size: 30,),
-                    ),
-                    label: '',
-                    ),
-      
-                  BottomNavigationBarItem(
-                    icon: Padding(padding: EdgeInsets.fromLTRB(0, 10.h, 0, 0),
-                    child: Icon(Icons.calendar_today_outlined, 
-                    color: Color.fromARGB(255, 68, 68, 68),
-                    size: 30,),
-                    ),
-                    label: '',
-                    ),
-      
-                  BottomNavigationBarItem(
-                    icon: Padding(padding: EdgeInsets.fromLTRB(0, 10.h, 0, 0),
-                    child: Icon(Icons.notifications_none_outlined, 
-                    color: Color.fromARGB(255, 68, 68, 68),
-                    size: 30,),
-                    ),
-                    label: '',
-                    ),
-      
-                  BottomNavigationBarItem(
-                    icon: Padding(padding: EdgeInsets.fromLTRB(0, 10.h, 0, 0),
-                    child: Icon(Icons.grid_view_outlined, 
-                    color: Color.fromARGB(255, 68, 68, 68),
-                    size: 30,),
-                    ),
-                    label: '',
-                    ),
-                ], 
-                type: BottomNavigationBarType.fixed,
-                ),
-            floatingActionButton: 
-              FloatingActionButton(
-                onPressed: () => Provider.of<ToDoListProvider>(context, listen: false).addItem(context),
-                child: Icon(Icons.add, color: Colors.white, size: 40,),
-                backgroundColor: Color.fromARGB(255, 223, 189, 67),
-                elevation: 0,
-      
-                ),
-            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-            
-          ),
+    return ScreenUtilInit(
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Center(child: pages[pageIndex]),
+          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+          bottomNavigationBar: 
+            BottomNavigationBar(
+              onTap: (index) => setState(() {
+                pageIndex = index;
+              }),
+              currentIndex: pageIndex,
+              backgroundColor: Color.fromARGB(255, 237, 234, 234),
+              items: [
+                BottomNavigationBarItem(
+                  icon: Padding(padding: EdgeInsets.fromLTRB(0, 10.h, 0, 0),
+                  child: Icon(Icons.home, 
+                  color: Color.fromARGB(255, 68, 68, 68),
+                  size: 30,),
+                  ),
+                  label: '',
+                  ),
+    
+                BottomNavigationBarItem(
+                  icon: Padding(padding: EdgeInsets.fromLTRB(0, 10.h, 0, 0),
+                  child: Icon(Icons.calendar_today_outlined, 
+                  color: Color.fromARGB(255, 68, 68, 68),
+                  size: 30,),
+                  ),
+                  label: '',
+                  ),
+    
+                BottomNavigationBarItem(
+                  icon: Padding(padding: EdgeInsets.fromLTRB(0, 10.h, 0, 0),
+                  child: Icon(Icons.notifications_none_outlined, 
+                  color: Color.fromARGB(255, 68, 68, 68),
+                  size: 30,),
+                  ),
+                  label: '',
+                  ),
+    
+                BottomNavigationBarItem(
+                  icon: Padding(padding: EdgeInsets.fromLTRB(0, 10.h, 0, 0),
+                  child: Icon(Icons.grid_view_outlined, 
+                  color: Color.fromARGB(255, 68, 68, 68),
+                  size: 30,),
+                  ),
+                  label: '',
+                  ),
+              ], 
+              type: BottomNavigationBarType.fixed,
+              ),
+          floatingActionButton: 
+            FloatingActionButton(
+              onPressed: () => Provider.of<ToDoListProvider>(context, listen: false).addItem(context),
+              child: Icon(Icons.add, color: Colors.white, size: 40,),
+              backgroundColor: Color.fromARGB(255, 223, 189, 67),
+              elevation: 0,
+    
+              ),
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          
         ),
-        designSize: const Size(428, 926),
       ),
+      designSize: const Size(428, 926),
     );
     
   }
