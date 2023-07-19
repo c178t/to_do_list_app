27c46ff5bf8a249d2f4d3b1ee27965874290e78e
Cian Thomas <cianticloud@gmail.com>
2023-07-19 10:18:12 -0400

    fixed time/moved bottomsheet to main

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String setTime = '';
+
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+
+  bool timePressed = false;
+  
+
+  final _textController = TextEditingController();
+
   int pageIndex = 0;
   final pages = [HomePage(), CalendarPage(), NotiPage(), GroupPage()];
 
@@ -88,7 +97,201 @@ class _MyAppState extends State<MyApp> {
               ),
           floatingActionButton: 
             FloatingActionButton(
-              onPressed: () => Provider.of<ToDoListProvider>(context, listen: false).addItem(context),
+              onPressed: () {
+
+                      showModalBottomSheet(
+            backgroundColor: Color.fromARGB(255, 255, 253, 244),
+            isScrollControlled: true,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(15)
+              ) 
+            ),
+            context:  context, 
+            builder: (context) {
+            return StatefulBuilder(
+              builder:  (context, setState) {
+                return SingleChildScrollView(
+                  child: Container(
+                   padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+                   child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    
+                    children: [
+                        
+                      Row(
+                        children: [
+                          Container(
+                            width: 300,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "New Item", 
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 68, 68, 68)
+                                ), 
+                                
+                              ),
+                            ),
+                          ),
+                        
+                          Text(
+                            setTime,
+                            textAlign: TextAlign.end,
+                            style: TextStyle(
+                              fontSize: 20
+                            ),
+                          )
+                        ],
+                      ),
+                        
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          controller: _textController,
+                          cursorColor: Color.fromARGB(255, 223, 189, 67),
+                          decoration: const InputDecoration(
+                            hintText: 'To Do...',
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: Color.fromARGB(255, 223, 189, 67),
+                                style: BorderStyle.solid,
+                                
+                              )
+                            )
+                            )
+                          ),
+                      
+                        ),
+                        
+                        SizedBox(
+                          width: 350,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStatePropertyAll(
+                                                Colors.white
+                                                )
+                            ),
+                          
+                          onPressed: () {
+                        
+                            timePressed = true;
+                        
+                            showTimePicker(
+                                  initialEntryMode: TimePickerEntryMode.inputOnly,
+                                  context: context, 
+                                  initialTime: TimeOfDay.now()
+                                ).then((value) {
+                                  setState(() {
+                                    _timeOfDay = value!;
+                                    setTime = _timeOfDay.format(context).toString();
+                                  });
+                                  
+                                  
+                                });
+            
+                            
+            
+                            
+            
+                          }, 
+                          child: Text(
+                            "Add Custom Time",
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 68, 68, 68)
+                            ),
+                          )
+                          ),
+                        ),
+                        
+                        SizedBox(
+                          width: 350,
+                          child: Padding(
+                            padding: const EdgeInsets.fromLTRB(0,0,0,32),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                      
+                                if (_textController.text == '') {
+                                  showDialog(
+                                    context: context, 
+                                    builder: (context) => AlertDialog(
+                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                                      title: Text('Name of item cannot be empty'),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () => Navigator.pop(context), 
+                                          child: Text('OK')
+                                          )
+                                      ],
+                                      
+                                    ),
+                                  );
+                                } else {
+                                    Provider.of<ToDoListProvider>(context, listen: false).toDoItems.insert(
+                                    0,
+                                    {
+                                      "time" : timePressed? _timeOfDay.format(context).toString(): '',
+                                      "name" : _textController.text,
+                                    "isChecked" : false,
+                                      "isBool" : false
+                                    }
+                                  );
+                                        
+                            
+            
+            
+                                Provider.of<ToDoListProvider>(context, listen: false).itemCount++;
+                                
+                                timePressed = false;
+            
+                                setTime = '';
+                          
+                                _timeOfDay = TimeOfDay.now();
+                          
+                                _textController.clear();   
+                                
+                                Navigator.pop(context);
+                                
+                                Provider.of<ToDoListProvider>(context, listen: false).notify();
+                                }
+                                      
+                                
+                                      
+                              
+                              }, 
+                              child: Icon(
+                                
+                                Icons.done
+                              ),
+                              style: ButtonStyle(
+                                
+                                backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 223, 189, 67))
+                              ),
+                              ),
+                          ),
+                        ),
+                      
+                        
+                    ],
+                  ),
+                ),
+              );
+              },
+              
+              
+              
+              
+               
+            );
+            },
+          );
+
+            
+            
+            
+              },
               child: Icon(Icons.add, color: Colors.white, size: 40,),
               backgroundColor: Color.fromARGB(255, 223, 189, 67),
               elevation: 0,
--- lib/provider.dart
+++ lib/provider.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 
 class ToDoListProvider extends ChangeNotifier {
 
-String setTime = '';
+  int itemCount = 7; 
 
-TimeOfDay _timeOfDay = TimeOfDay.now();
-
-bool timePressed = false;
- int itemCount = 7;
-
-final _textController = TextEditingController();
+ 
 
  final List<Map<String, dynamic>> toDoItems = [
     {
@@ -64,177 +59,9 @@ final _textController = TextEditingController();
 
   ToDoListProvider();
 
-  void addItem(BuildContext context) {
-
-
-    showModalBottomSheet(
-      backgroundColor: Color.fromARGB(255, 255, 253, 244),
-      isScrollControlled: true,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(15)
-        ) 
-      ),
-      context:  context, 
-      builder: (context) {
-       return SingleChildScrollView(
-         child: Container(
-           padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-           child: Column(
-            mainAxisSize: MainAxisSize.min,
-            
-            children: [
-                
-              Row(
-                children: [
-                  Container(
-                    width: 300,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        "New Item", 
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
-                          color: Color.fromARGB(255, 68, 68, 68)
-                        ), 
-                        
-                      ),
-                    ),
-                  ),
-                
-                  Text(
-                    setTime,
-                    textAlign: TextAlign.end,
-                    style: TextStyle(
-                      fontSize: 20
-                    ),
-                  )
-                ],
-              ),
-                
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _textController,
-                  cursorColor: Color.fromARGB(255, 223, 189, 67),
-                  decoration: const InputDecoration(
-                    hintText: 'To Do...',
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 223, 189, 67),
-                        style: BorderStyle.solid,
-                        
-                      )
-                    )
-                    )
-                  ),
-              
-                ),
-                
-                SizedBox(
-                  width: 350,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(
-                                        Colors.white
-                                        )
-                    ),
-                  
-                  onPressed: () {
-                
-                    timePressed = true;
-                
-                    showTimePicker(
-                          initialEntryMode: TimePickerEntryMode.inputOnly,
-                          context: context, 
-                          initialTime: TimeOfDay.now()
-                        ).then((value) {
-                          
-                          _timeOfDay = value!;
-                        });
-                  }, 
-                  child: Text(
-                    "Add Custom Time",
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 68, 68, 68)
-                    ),
-                  )
-                  ),
-                ),
-                
-                SizedBox(
-                  width: 350,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0,0,0,32),
-                    child: ElevatedButton(
-                      onPressed: () {
-                              
-                        if (_textController.text == '') {
-                          showDialog(
-                            context: context, 
-                            builder: (context) => AlertDialog(
-                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                              title: Text('Name of item cannot be empty'),
-                              actions: [
-                                TextButton(
-                                  onPressed: () => Navigator.pop(context), 
-                                  child: Text('OK')
-                                  )
-                              ],
-                              
-                            ),
-                          );
-                        } else {
-                            toDoItems.insert(
-                            0,
-                            {
-                              "time" : timePressed? _timeOfDay.format(context).toString(): '',
-                              "name" : _textController.text,
-                            "isChecked" : false,
-                              "isBool" : false
-                            }
-                          );
-                                
-                        if (timePressed) {
-                          setTime = _timeOfDay.format(context).toString();
-                        }
-
-
-                        itemCount++;
-                        
-                        timePressed = false;
-                  
-                        _timeOfDay = TimeOfDay.now();
-                  
-                        _textController.clear(); 
-                        
-                        Navigator.pop(context);
-                        }
-                              
-                        
-                              
-                       notifyListeners();
-                      }, 
-                      child: Icon(
-                        
-                        Icons.done
-                      ),
-                      style: ButtonStyle(
-                        
-                        backgroundColor: MaterialStatePropertyAll(Color.fromARGB(255, 223, 189, 67))
-                      ),
-                      ),
-                  ),
-                ),
-               
-                
-            ],
-           ),
-         ),
-       );
-      },
-    );
+  void addItem() {
+    
+    notifyListeners();
 
   }
 
